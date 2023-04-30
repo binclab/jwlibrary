1a491a0b0c4c4481d1ed8aa63d012787e5f29b3e
bretantonio <bretjoseph@gmail.com>
2023-04-30 12:12:53 +0200

    Update Webkit

--- gui/winstack.c
+++ gui/winstack.c
@@ -2,10 +2,10 @@
 
 void init_winstack()
 {
-    GListModel *model = gtk_map_list_model_new
-    GtkSingleSelection * selection = gtk_single_selection_new(model);
+    GtkStringList *model = gtk_string_list_new(NULL);
+    GtkListItemFactory *factory = gtk_signal_list_item_factory_new();
     winstack = gtk_stack_new();
-    medialibrary = gtk_grid_view_new(); //gtk_grid_view_new();
+    medialibrary = gtk_grid_view_new((GtkSelectionModel*)model, factory);
     gtk_paned_set_end_child((GtkPaned*)paned, winstack);
     gtk_paned_set_shrink_end_child((GtkPaned*)paned, FALSE);
     gtk_paned_set_resize_end_child((GtkPaned*)paned, TRUE);
