bca94e8666982af961acb6b510d050ca562c1b01
Bret Joseph Antonio <bretjoseph@gmail.com>
2024-10-30 16:55:02 +0200

    update

--- gui/drawer.c
+++ gui/drawer.c
@@ -1,17 +1,18 @@
 #include "drawer.h"
 
-GtkWidget *create_drawer()
+GtkWidget *create_drawer(GtkStack *winstack)
 {
-    drawer = gtk_center_box_new();
-    /*GtkWidget *switcher = gtk_stack_switcher_new();
-    gtk_stack_switcher_set_stack((GtkStackSwitcher*)switcher, (GtkStack*)winstack);
-    gtk_center_box_set_start_widget((GtkCenterBox*)drawer, switcher);
-    gtk_orientable_set_orientation((GtkOrientable*)drawer, GTK_ORIENTATION_VERTICAL);
+    drawer = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_widget_set_size_request(drawer, 100, -1);
-    instantiate_drawer();
+    /*instantiate_drawer();
     set_drawer_icons();
     connect_drawer_signals();
     populate_drawer();
     g_signal_connect(navbtn, "clicked", (GCallback)show_drawer, NULL);*/
+
+    GtkGesture *dragger = gtk_gesture_drag_new();
+    g_signal_connect(dragger, "drag-end", (GCallback)move_handle, gtk_widget_get_parent(drawer));
+    gtk_widget_add_controller(drawer, (GtkEventController *)dragger);
+
     return drawer;
 }
