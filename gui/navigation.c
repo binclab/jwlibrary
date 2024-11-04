702a8ec6e14cb4ebb28b95939c254296a1acf4ab
Bret Joseph Antonio <bretjoseph@gmail.com>
2024-11-04 06:37:15 +0200

    update

---
+++ gui/navigation.c
@@ -0,0 +1,66 @@
+#include "navigation.h"
+
+void *init_navigation(GtkGrid *grid, GtkStack *stack) {
+  GtkWidget *header = gtk_action_bar_new();
+  GtkWidget *historybtn = gtk_button_new_from_icon_name("document-open-recent-symbolic");
+  GtkWidget *syncbtn = gtk_button_new_from_icon_name("emblem-synchronizing-symbolic");
+  GtkWidget *settingsbtn = gtk_button_new_from_icon_name("emblem-system-symbolic");
+  GtkWidget *morebtn = gtk_button_new_from_icon_name("view-more-symbolic");
+  GtkWidget *navinfo = gtk_label_new("Home");
+  switcher = gtk_stack_switcher_new();
+
+  /*navbar = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  instantiate_navbar();
+  set_navbar_icons();
+  connect_navbar_signals();
+  populate_navbar();*/
+
+  gtk_orientable_set_orientation((GtkOrientable *)switcher,
+                                 GTK_ORIENTATION_VERTICAL);
+
+  gtk_stack_switcher_set_stack((GtkStackSwitcher *)switcher, stack);
+  gtk_action_bar_pack_start((GtkActionBar*)header, navinfo);
+  gtk_action_bar_pack_end((GtkActionBar*)header, morebtn);
+  gtk_action_bar_pack_end((GtkActionBar*)header, settingsbtn);
+  gtk_action_bar_pack_end((GtkActionBar*)header, syncbtn);
+  gtk_action_bar_pack_end((GtkActionBar*)header, historybtn);
+  gtk_widget_set_hexpand(header, TRUE);
+  gtk_widget_set_valign(switcher, GTK_ALIGN_START);
+  gtk_grid_attach(grid, switcher, 0, 1, 1, 1);
+  gtk_grid_attach(grid, header, 1, 0, 1, 1);
+}
+
+static void instantiate_navbar() {
+  space = gtk_label_new(NULL);
+  navopenimg = gtk_image_new();
+  navcloseimg = gtk_image_new();
+
+  navbtn = gtk_button_new();
+  homebtn = gtk_button_new();
+  biblebtn = gtk_button_new();
+  publicbtn = gtk_button_new();
+  mediabtn = gtk_button_new();
+  meetingsbtn = gtk_button_new();
+  wolbtn = gtk_button_new();
+  playerbtn = gtk_button_new();
+  settingsbtn = gtk_button_new();
+}
+
+static void set_navbar_icons() {
+  gtk_button_set_child((GtkButton *)navbtn, navopenimg);
+}
+
+static void connect_navbar_signals() { gtk_widget_set_vexpand(space, TRUE); }
+
+static void populate_navbar() {
+  gtk_box_append((GtkBox *)navbar, navbtn);
+  gtk_box_append((GtkBox *)navbar, homebtn);
+  gtk_box_append((GtkBox *)navbar, biblebtn);
+  gtk_box_append((GtkBox *)navbar, publicbtn);
+  gtk_box_append((GtkBox *)navbar, mediabtn);
+  gtk_box_append((GtkBox *)navbar, meetingsbtn);
+  gtk_box_append((GtkBox *)navbar, wolbtn);
+  gtk_box_append((GtkBox *)navbar, playerbtn);
+  gtk_box_append((GtkBox *)navbar, space);
+  gtk_box_append((GtkBox *)navbar, settingsbtn);
+}
