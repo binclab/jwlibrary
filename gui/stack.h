39c1460c0269a1d9a829d29fa63dcd404d4bb98b
Bret Joseph Antonio <bretjoseph@gmail.com>
2024-11-07 08:14:54 +0200

    update

--- gui/stack.h
+++ gui/stack.h
@@ -4,10 +4,15 @@
 #include <webkit/webkit.h>
 
 static GtkStackPage *page[8];
-static void init_home(GtkStack *stack), init_bible(GtkStack *stack), init_publications(GtkStack *stack), init_media(GtkStack *stack);
+static void init_home(GtkStack *stack), init_bible(GtkStack *stack), init_library(GtkStack *stack), init_media(GtkStack *stack);
 static void init_meetings(), init_wol(), init_player(), init_settings();
 static void show_sidebar(GtkToggleButton *button, GtkWidget *revealer);
 extern gchar *home;
 extern void init_navigation(GtkGrid *grid, GtkStack *stack);
+
+typedef struct {
+    
+} Pages;
+
 
 #endif  // WINSTACK_H_INCLUDED
